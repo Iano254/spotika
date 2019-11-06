0433a6f743f930ef095c72307c094236f971a929
iano <oidancun@gmail.com>
2019-11-07 02:44:34 +0300

    m

---
+++ components/header.php
@@ -0,0 +1,98 @@
+<header style="margin-bottom:15px;">
+<div class="login-section">
+    
+    <div class="logo-section">
+        <img src="png/" style="height:45px" alt="">
+    </div>
+    <div class="btn-section" style="display:flex; justify-content:space-between">
+    <div class="logn " style="margin-right:10px;">
+    <button style="border-radius:0px" type="submit" class="btn btn-default login-btn">LOGIN</button>
+    </div>
+    <div class="sgnp ">
+    <button style="border-radius:0px" type="submit" class="btn btn-default reg-btn">REGISTER</button>
+    </div>
+    </div>
+    
+</div>
+<!-- <div class="main-nav">
+<nav class="navbar navbar-expand-lg navbar-light bg-black">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent" style="padding-left:15px; padding-right:15px;">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Sports <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Live Games</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Jackpots</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Live Scores</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Statistics</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Results</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">News</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">App</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Help</a>
+      </li>
+      
+    </ul>
+    
+  </div>
+</nav>
+</div> -->
+
+
+<div class="ball">
+<nav class="navbar navbar-expand-lg navbar-light bg-maroon" > 
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent"  style="padding-left:15px; padding-right:15px;">
+    <ul class="navbar-nav mr-auto">
+      
+    <li class="nav-item active">
+        <a class="nav-link" href="#"><span class="fa fa-arrow-circle-down fa-lg" style="margin-right:10px; color:#F7CD00;"></span>Football</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span class="fa fa-arrow-circle-down fa-lg" style="margin-right:10px; color:#F7CD00;"></span>Basketball</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span class="fa fa-arrow-circle-down fa-lg" style="margin-right:10px; color:#F7CD00;"></span>Tennis</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span class="fa fa-arrow-circle-down fa-lg" style="margin-right:10px; color:#F7CD00;"></span>Rugby Union</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span class="fa fa-arrow-circle-down fa-lg" style="margin-right:10px; color:#F7CD00;"></span>Boxing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span class="fa fa-arrow-circle-down fa-lg" style="margin-right:10px; color:#F7CD00;"></span>Cricket</a>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0" style="display:none">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" style="border-radius:0px;">
+</form>
+      
+  </div>
+</nav>
+</div>
+
+</header>
+
---
+++ components/markets/highlights.php
@@ -0,0 +1,270 @@
+<div class="strip margin-mobi shadow-sm" style="box-shadow: 0 .125rem .25rem rgba(0,0,0,.075)!important; border-radius:4px; overflow:hidden!important"">
+    <div class="heading-t" style="overflow:hidden!important">
+        <span class="fa fa-soccer-ball-o" style="margin-right:5px; color:white"></span>
+        <span style="color:white;">Thursday 20th Oct</span>
+    </div>
+    <div class="form-t" style="display:flex; align-items: left; padding-left:15px; padding-right:15px; padding-top:8px;">
+        <div class="input-f" >
+            <div class="dropdown" style="margin-right:15px; ">
+                <button style=" color:black; border-color:" class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Today Games
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+                
+            </div>
+            <div class="dropdown"style="margin-right:15px; ">
+                <button style=" color:black; border-color:" class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Leagues
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+                
+            </div>
+            <div class="dropdown"style="margin-right:15px; ">
+                <button style=" color:black; border-color:" class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Country
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+                
+            </div>
+
+            
+            
+        </div>
+    </div>
+</div>
+
+<div class="odds">
+    
+
+    <div class="odd-sec" style="margin-bottom:10px;">
+        <div class="meta-sec" style="display:flex; justify-content: space-between;padding-left:15px; padding-right:15px; padding-top:10px; padding-bottom:10px;">
+            <div class="league">
+                <span style="font-weight:bold;">PREMIERE LEAGUE</span>
+            </div>
+            <div class="action">
+            <a href="./more.php"><span class="fa fa-plus" style=""></span></a>
+            </div>
+        </div>
+            
+            <div class="row">
+
+                <div class="col-md-2" style="padding:0px 5px;">
+                    <table style="width:100%">
+                        <tr>
+                            <td style="padding-left:10px;">
+                                <span class="meta-t yellow-txt">14:30 26/07/19 ID: 1306</span><br>
+                                <span class="meta-t">UNITED vs CITY</span><br>
+                                <span class="meta-t yellow-txt">0 : 0</span><br>
+
+                            </td>
+                            
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-md-3 margin-mobi" style=" padding:0px 5px;">
+                    <div class="odd-selector-wrapper">
+                        <table class="" style="width:100%">
+                        <tr>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">HOME</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">DRAW</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">AWAY</span></td>
+                        </tr>
+                        </table>
+                        <table class="" style="width:100% ">
+                            <tr>
+                                <td class="odd-selector" style="border-radius:7px; margin-right:1px; border-right:1px solid transparent;">2.91</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent; background-color:#F7CD00">3.60</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">2.23</td>
+                            </tr>
+                        </table>
+                    </div>  
+                </div>
+                <div class="col-md-3 hide-mobi" style=" padding:0px 5px;">
+                    <div class="odd-selector-wrapper">
+                    <table class="" style="width:100%">
+                        <tr>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">1ORX</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">XOR2</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">1OR2</span></td>
+                        </tr>
+                        </table>
+                        <table style="width:100%">
+                            <tr>
+                                <td class="odd-selector" style="border-radius:7px; margin-right:1px; border-right:1px solid transparent;">2.91</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent; ">3.60</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">2.23</td>
+                            </tr>
+                        </table>
+                    </div>  
+                </div>
+                <div class="col-md-2 hide-mobi" style=" padding:0px 5px;">
+                    <div class="odd-selector-wrapper" style="">
+                        <table style="width:100%">
+                        <tr>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">OVER</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">UNDER</span></td>
+                        </tr>
+                        </table>
+                        <table style="width:100%">
+                            <tr>
+                                <td class="odd-selector" style="border-radius:7px; border-right:1px solid transparent;">2.91</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">2.23</td>
+                            </tr>
+                        </table>
+                        
+                    </div>  
+                </div>
+                <div class="col-md-2 hide-mobi" style=" padding:0px 5px;">
+                    <div class="odd-selector-wrapper" style="">
+                        <table style="width:100%">
+                        <tr>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">GG</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">NG</span></td>
+                        </tr>
+                        </table>
+                        <table style="width:100%">
+                            <tr>
+                                <td class="odd-selector" style="border-radius:7px; border-right:1px solid transparent;">2.91</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">2.23</td>
+                            </tr>
+                        </table>
+                        
+                    </div>  
+                </div>
+                
+                
+            </div>
+
+            
+        
+        
+    
+    </div>
+
+
+
+
+
+    <div class="odd-sec" style="margin-bottom:10px;">
+        <div class="meta-sec" style="display:flex; justify-content: space-between;padding-left:15px; padding-right:15px; padding-top:10px; padding-bottom:10px;">
+            <div class="league">
+                <span style="font-weight:bold;">PREMIERE LEAGUE</span>
+            </div>
+            <div class="action">
+            <a href="./more.php"><span class="fa fa-plus" style=""></span></a>
+            </div>
+        </div>
+            
+            <div class="row">
+
+                <div class="col-md-2" style="padding:0px 5px;">
+                    <table style="width:100%">
+                        <tr>
+                            <td style="padding-left:10px;">
+                                <span class="meta-t yellow-txt">14:30 26/07/19 ID: 1306</span><br>
+                                <span class="meta-t">UNITED vs CITY</span><br>
+                                <span class="meta-t yellow-txt">0 : 0</span><br>
+
+                            </td>
+                            
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-md-3 margin-mobi" style=" padding:0px 5px;">
+                    <div class="odd-selector-wrapper">
+                        <table class="" style="width:100%">
+                        <tr>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">HOME</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">DRAW</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">AWAY</span></td>
+                        </tr>
+                        </table>
+                        <table class="" style="width:100% ">
+                            <tr>
+                                <td class="odd-selector" style="border-radius:7px; margin-right:1px; border-right:1px solid transparent;">2.91</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">3.60</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">2.23</td>
+                            </tr>
+                        </table>
+                    </div>  
+                </div>
+                <div class="col-md-3 hide-mobi" style=" padding:0px 5px;">
+                    <div class="odd-selector-wrapper">
+                    <table class="" style="width:100%">
+                        <tr>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">1ORX</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">XOR2</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">1OR2</span></td>
+                        </tr>
+                        </table>
+                        <table style="width:100%">
+                            <tr>
+                                <td class="odd-selector" style="border-radius:7px; margin-right:1px; border-right:1px solid transparent;">2.91</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent; background-color:#F7CD00">3.60</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">2.23</td>
+                            </tr>
+                        </table>
+                    </div>  
+                </div>
+                <div class="col-md-2 hide-mobi" style=" padding:0px 5px;">
+                    <div class="odd-selector-wrapper" style="">
+                        <table style="width:100%">
+                        <tr>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">OVER</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">UNDER</span></td>
+                        </tr>
+                        </table>
+                        <table style="width:100%">
+                            <tr>
+                                <td class="odd-selector" style="border-radius:7px; border-right:1px solid transparent;">2.91</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">2.23</td>
+                            </tr>
+                        </table>
+                        
+                    </div>  
+                </div>
+                <div class="col-md-2 hide-mobi" style=" padding:0px 5px;">
+                    <div class="odd-selector-wrapper" style="">
+                        <table style="width:100%">
+                        <tr>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">GG</span></td>
+                            <td style="text-align:center;"><span class="htts" style="text-align:center;">NG</span></td>
+                        </tr>
+                        </table>
+                        <table style="width:100%">
+                            <tr>
+                                <td class="odd-selector" style="border-radius:7px; border-right:1px solid transparent;">2.91</td>
+                                <td class="odd-selector" style="border-radius:7px; border-left:1px solid transparent;">2.23</td>
+                            </tr>
+                        </table>
+                        
+                    </div>  
+                </div>
+                
+                
+            </div>
+
+            
+        
+        
+    
+    </div>
+
+    
+</div>
+
+
+
+
